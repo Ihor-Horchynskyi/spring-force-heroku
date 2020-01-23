e3189bae19462cf799ba843c9ddbefec412e2d4f
Ihor Horchynskyi <ihor.horchynskyi@koroka.solutions>
2020-01-23 18:06:28 +0200

    Init

---
+++ src/main/java/com/koroka/herokuIntegration/HerokuIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.koroka.herokuIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/koroka/herokuIntegration/HerokuIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koroka.herokuIntegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

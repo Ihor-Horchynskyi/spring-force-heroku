745f09a9d180030a5c8700735c5f09a5363e771c
Ihor Horchynskyi <ihor.horchynskyi@koroka.solutions>
2020-01-24 14:58:14 +0200

    Status endpoint added

---
+++ src/main/java/com/koroka/herokuIntegration/controllers/StatusController.java
@@ -0,0 +1,14 @@
+package com.koroka.herokuIntegration.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatusController {
+
+    @RequestMapping("/status")
+    String getStatus() {
+        return "test status";
+    }
+}

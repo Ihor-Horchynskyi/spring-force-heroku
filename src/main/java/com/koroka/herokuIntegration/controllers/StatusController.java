1810ccdaea00779717d0b9e4a6b7429ea107558d
Ihor Horchynskyi <ihor.horchynskyi@koroka.solutions>
2020-01-24 16:46:11 +0200

    some buttons

--- src/main/java/com/koroka/herokuIntegration/controllers/StatusController.java
+++ src/main/java/com/koroka/herokuIntegration/controllers/StatusController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class StatusController {
 
-    @GetMapping("/pages/status")
+    @GetMapping("/status")
     String getStatus(Model model) {
         return "pages/status";
     }
